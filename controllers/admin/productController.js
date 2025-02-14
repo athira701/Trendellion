76c7a2104f183d51c5cb4b47ff08cbb0ae3a958e
athira701 <prakashathira97@gmail.com>
2025-02-14 21:20:23 +0530

    bug fixed

--- controllers/admin/productController.js
+++ controllers/admin/productController.js
@@ -16,7 +16,7 @@ const getProductPage = async (req, res) => {
 
         const totalProducts = await Product.countDocuments();
         const products = await Product.find({})
-            .select('productName productImage totalStock category salePrice  isBlocked')
+            .select('productName productImage quantity category salePrice color isBlocked')
             .populate('category') // Populate only the 'name' field of category
             .sort({ createdAt: -1 })
             .skip(skip)
@@ -175,10 +175,9 @@ const addProduct = async (req, res) => {
         }
 
         // Check image uploads
-        if (!files || files.length < 1) {
-            validationErrors.push('Please upload at least one image.');
+        if (!files || files.length < 4) {
+            validationErrors.push('Please upload at least 4 images.');
         }
-
 
         console.log("reached ")
 
